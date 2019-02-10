570c97c040087d56b5b9b01f9d746e1bba01a8c1
unknown <mhbitarafan1375@gmail.com>
2019-02-10 20:45:49 +0330

    pc

--- src/app/AppSettings.ts
+++ src/app/AppSettings.ts
@@ -1,8 +1,4 @@
 export class AppSettings {
-<<<<<<< HEAD
   public static base_host = 'http://192.168.1.110/prestashop';
-=======
-  public static base_host = 'http://127.0.1.1:8080/prestashop';
->>>>>>> e948cd884f0ccb52ec955ad4960b127e4086550b
   public static api_key = '7IV1SY44MI46SZKS5E5VABURC3KEPVXZ';
 }
